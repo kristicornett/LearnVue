89b18b00b51d2a58050fc7f07a79b4d46ded4912
Kristina Cornett <kcornett@precisedevnet.com>
2023-10-23 11:13:57 -0500

    conditional and list rendering

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ const app = Vue.createApp({
             description: 'The most comfy socks on the planet.',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.fridaysocks.com/blogs/news/the-ultimate-guide-to-womens-socks',
-            inStock: false
+            inventory: 10,
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [ 'xs', 'small', 'medium', 'large', 'x-large']
         }
     }
 })
