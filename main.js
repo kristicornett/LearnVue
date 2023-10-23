2738fc7b27ac6e686eba1d56f8a28abf032763a5
Kristina Cornett <kcornett@precisedevnet.com>
2023-10-23 13:10:24 -0500

    Class & style binding, computed properties

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             description: 'The most comfy socks on the planet.',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: 'https://www.fridaysocks.com/blogs/news/the-ultimate-guide-to-womens-socks',
-            inventory: 10,
-            onSale: false,
+            brand: 'Vue Mastery',
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             sizes: [ 'xs', 'small', 'medium', 'large', 'x-large']
         }
@@ -21,13 +21,32 @@ const app = Vue.createApp({
                 this.cart += 1
             },
 
-            updateImage(variantImage){
-                this.image = variantImage
+            updateVariant(index){
+                this.selectedVariant = index
+                
             },
 
             removeFromCart(){
                 this.cart -= 1
             }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product
+            },
+            image(){
+                return this.variants[this.selectedVariant].image
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].quantity
+            },
+            sale(){
+                if (this.onSale)
+                {
+                return this.brand + ' ' + this.product + 'is on sale!' 
+                }
+                return ''
+            }  
         }
     }
 )
