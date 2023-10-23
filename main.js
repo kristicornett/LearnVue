9b75defb3fda04048d50eb96f85b7c633a6f469c
Kristina Cornett <kcornett@precisedevnet.com>
2023-10-23 11:35:04 -0500

    Event handling in vue

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data(){
         return{
+            cart: 0,
             product: 'Socks',
             description: 'The most comfy socks on the planet.',
             image: './assets/images/socks_green.jpg',
@@ -9,10 +10,27 @@ const app = Vue.createApp({
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: [ 'xs', 'small', 'medium', 'large', 'x-large']
         }
+        },
+        methods: {
+            addToCart(){
+                this.cart += 1
+            },
+
+            updateImage(variantImage){
+                this.image = variantImage
+            },
+
+            removeFromCart(){
+                this.cart -= 1
+            }
+        }
     }
-})
+)
+
+
+
