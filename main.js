5c7f6e5d44486beefa5be7f3576c3672f8d3b4e4
Kristina Cornett <kcornett@precisedevnet.com>
2023-10-23 10:52:57 -0500

    understanding v-bind, v-if, v-else, v-show and adding to the object

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'The most comfy socks on the planet.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.fridaysocks.com/blogs/news/the-ultimate-guide-to-womens-socks',
+            inStock: false
+        }
+    }
+})
